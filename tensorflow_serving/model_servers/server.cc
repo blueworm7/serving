ba1dcfc1b4f9552c78683b543a28303427637c7b
chaeno <anicetus0417@gmail.com>
2020-07-20 01:40:03 +0000

    test Commit is adopted now

--- tensorflow_serving/model_servers/server.cc
+++ tensorflow_serving/model_servers/server.cc
@@ -375,7 +375,7 @@ Status Server::BuildAndStart(const Options& server_options) {
           server_options.http_timeout_in_ms, monitoring_config,
           server_core_.get());
       if (http_server_ != nullptr) {
-        LOG(INFO) << "Exporting HTTP/REST API at:" << server_address << " ...";
+        LOG(INFO) << "Blueworm7 Build Test Exporting HTTP/REST API at:" << server_address << " ...";
       } else {
         LOG(ERROR) << "Failed to start HTTP Server at " << server_address;
       }
